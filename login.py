223594beeb9493651b45469fdceeb9a5d648e1fd
idmasse <idmasse@icloud.com>
2025-05-05 11:39:24 -0700

    updates login xpath's

--- login.py
+++ login.py
@@ -8,8 +8,8 @@ import time
 
 load_dotenv()
 
-def wait_to_click(driver, by, value, timeout=30):
-    browser_wait = WebDriverWait(driver, timeout)
+def wait_to_click(driver, by, value):
+    browser_wait = WebDriverWait(driver, 30)
     return browser_wait.until(EC.element_to_be_clickable((by, value)))
 
 def dismiss_popup(driver):
@@ -24,25 +24,31 @@ def dismiss_popup(driver):
     except Exception:
         pass
 
+def accept_cookies(driver):
+    browser_wait = WebDriverWait(driver, 30)
+    try:
+        browser_wait.until(EC.presence_of_element_located((By.ID, "usercentrics-cmp-ui")))
+        shadow_host = driver.find_element(By.ID, "usercentrics-cmp-ui")
+        shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
+        accept_button = shadow_root.find_element(By.ID, "accept")
+        accept_button.click()
+        print('cookie popup accepted')
+    except Exception:
+        pass
+
 def italist_login(driver, username, password):
     LOGIN_URL = os.getenv('LOGIN_URL')
     driver.get(LOGIN_URL)
     time.sleep(5)
 
-    browser_wait = WebDriverWait(driver, 30)
-    
-    browser_wait.until(EC.presence_of_element_located((By.ID, "usercentrics-cmp-ui")))
-    shadow_host = driver.find_element(By.ID, "usercentrics-cmp-ui")
-    shadow_root = driver.execute_script("return arguments[0].shadowRoot", shadow_host)
-    accept_button = shadow_root.find_element(By.ID, "accept")
-    accept_button.click()
+    accept_cookies(driver)
     
     dismiss_popup(driver)
-    username_field = wait_to_click(driver, By.XPATH, '//*[@id="root"]/div[2]/div[2]/div[1]/form/div[3]/div/input')
+    username_field = wait_to_click(driver, By.XPATH, '/html/body/div[1]/div/div[2]/div[2]/div[1]/form/div[3]/div/div/input')
     username_field.send_keys(username)
 
     dismiss_popup(driver)
-    password_field = wait_to_click(driver, By.XPATH, '//*[@id="root"]/div[2]/div[2]/div[1]/form/div[5]/input')
+    password_field = wait_to_click(driver, By.XPATH, '//*[@id="root"]/div[2]/div[2]/div[1]/form/div[5]/div/input')
     password_field.send_keys(password)
 
     dismiss_popup(driver)
